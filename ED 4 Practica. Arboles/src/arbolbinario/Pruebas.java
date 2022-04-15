3932af519f0b4e7b38b41d3febde19828d3bf54e
Angel Garcia <a-garcia-alvarez@users.noreply.github.com>
2022-04-15 21:28:17 +0200

    Added P4 initial project

---
+++ ED 4 Practica. Arboles/src/arbolbinario/Pruebas.java
@@ -0,0 +1,9 @@
+package arbolbinario;
+
+public class Pruebas {
+
+    public static void main(String[] args) {
+        System.out.println("********** PRUEBAS ARBOL BINARIO DE EXPRESION **********");
+    }
+}
+
