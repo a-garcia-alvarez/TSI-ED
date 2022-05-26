b73e98cfb778a272da880af139e1291f18ef9bed
Angel Garcia <a-garcia-alvarez@users.noreply.github.com>
2022-05-26 23:34:16 +0200

    P4 2.2.* methods

--- ED 5 Practica. Grafos/src/Prueba.java
+++ ED 5 Practica. Grafos/src/Prueba.java
@@ -4,8 +4,78 @@ public class Prueba {
         final int numeroContactos = 10;
         Persona[] contactos = new Persona[numeroContactos];
         // Insertar personas de la red
+        Persona p0 = new Persona("Juan Nadie", "111111", "0");
+        Persona p1 = new Persona("Jose Cuervo", "222222", "1");
+        Persona p2 = new Persona("Eva Adan", "333333", "2");
+        Persona p3 = new Persona("Alicia Maravillas", "777777", "3");
+        Persona p4 = new Persona("Alan Turing", "999999", "4");
+        Persona p5 = new Persona("Guillermo Tell", "159267", "5");
+        Persona p6 = new Persona("Julio Catedrales", "123456", "6");
+        Persona p7 = new Persona("Lucas Pato", "231465", "7");
+        Persona p8 = new Persona("Paula Vazquez", "654321", "8");
+        Persona p9 = new Persona("Clara Oscuro", "123654", "9");
+
+        contactos[0] = p0;
+        contactos[1] = p1;
+        contactos[2] = p2;
+        contactos[3] = p3;
+        contactos[4] = p4;
+        contactos[5] = p5;
+        contactos[6] = p6;
+        contactos[7] = p7;
+        contactos[8] = p8;
+        contactos[9] = p9;
+
         Seguir_AETSISI g = new Seguir_AETSISI(numeroContactos,contactos);
         // Insertar relaciones Seguir_A
+        g.insertaRelacion(0,3);
+        g.insertaRelacion(3,0);
+        g.insertaRelacion(0,5);
+        g.insertaRelacion(5,0);
+        g.insertaRelacion(0,1);
+        g.insertaRelacion(1,4);
+        g.insertaRelacion(1,5);
+        g.insertaRelacion(5,4);
+        g.insertaRelacion(4,5);
+
+        g.insertaRelacion(6,8);
+        g.insertaRelacion(8,6);
+
+        g.insertaRelacion(2,9);
+        g.insertaRelacion(2,7);
+        g.insertaRelacion(7,9);
+        g.insertaRelacion(9,7);
+
         // Completar con todas las pruebas de los métodos del apartado 2.2.
+        System.out.println("Apartado 2.2.1. Número de grupos:");
+        System.out.println("Hay " + g.contarGrupos() + " grupos.");
+        System.out.println();
+
+        System.out.println("Apartado 2.2.2. Mostrar Seguidos por Guillermo directamente");
+        g.mostrarSeguidosPorADirectamente("Guillermo Tell");
+        System.out.println();
+
+        System.out.println("Apartado 2.2.3. Mostrar Seguidores directos de Juan");
+        g.mostrarSeguidoresDirectoDe("Juan Nadie");
+        System.out.println();
+
+        System.out.println("Apartado 2.2.4. pertenecen al mismo Grupo.");
+        g.sonDelMismoGrupo(p7,p9);
+        System.out.println();
+
+        System.out.println("Apartado 2.2.5. Mostrar personas mismo Grupo de Clara 9");
+        g.motrarMiembrosGrupo(p9);
+        System.out.println();
+
+        System.out.println("Apartado 2.2.6. Mostrar personas seguidas directa o\n indirectamente por Guillermo 5");
+        g.motrarSeguidosPor(p5);
+        System.out.println();
+
+        System.out.println("Apartado 2.2.7. Juan 0 sigue a Alicia 3");
+        System.out.println(g.mostrarEsSeguidaPor(p0.getNombre(), p3.getNombre()));
+        System.out.println();
+
+        System.out.println("Apartado 2.2.8. Seguidores de alicia");
+        g.mostrarSeguidoresDe(p3.getNombre());
     }
 }
--- ED 5 Practica. Grafos/src/Seguir_AETSISI.java
+++ ED 5 Practica. Grafos/src/Seguir_AETSISI.java
@@ -98,57 +98,117 @@ public void mostrar(){
 
     public int contarGrupos () {
         int resul = 0;
-       //Completar
+        int numV = getNumPersonas();
+        boolean[] visitados = new boolean[numV];
+        for (int i = 0; i < getNumPersonas(); i++)
+            visitados[i] = false;
+
+        for (int i = 0; i < numV; i++) {
+            if (!visitados[i]) {
+                miREd.recorridoEnProfundidad(i, visitados);
+                resul++;
+            }
+        }
         return resul;
     }
 
-
-
-
-
-
     // Apartado 2.2.2 Segundo método
     public void mostrarSeguidosPorADirectamente(String nombre) {
-        //Completar
+        if (devuelvePosNombre(nombre) >= 0) {
+            int pos = devuelvePosNombre(nombre);
+            System.out.printf("Seguidos por %s (%s):\n", nombre, pos);
+            for (int i = 0; i < getNumPersonas(); i++)
+                if (existeRelacion(pos, i))
+                    System.out.printf("%d: %s\n", i, contactos[i].getNombre());
+        } else
+            System.out.println("No existe alguien con ese nombre en la red");
     }
 
     // Apartado 2.2.3 Tercer método
     public void mostrarSeguidoresDirectoDe(String nombre) {
-        //Completar
+        if (devuelvePosNombre(nombre) >= 0) {
+            int pos = devuelvePosNombre(nombre);
+            System.out.printf("Seguidores de %s (%s):\n", nombre, pos);
+            for (int i = 0; i < getNumPersonas(); i++)
+                if (existeRelacion(i, pos))
+                    System.out.printf("%d: %s\n", i, contactos[i].getNombre());
+        } else
+            System.out.println("No existe alguien con ese nombre en la red");
     }
     // Apartado 2.2.4 Cuarto método
   public boolean sonDelMismoGrupo(Persona p, Persona p1){
-      //Completar
-      return true;
+      if (devuelvePosNombre(p.getNombre()) >= 0 && devuelvePosNombre(p1.getNombre()) >=0) {
+          boolean[] visitados = new boolean[getNumPersonas()];
+          for (int i = 0; i < getNumPersonas(); i++)
+              visitados[i] = false;
+          miREd.recorridoEnProfundidad(devuelvePosNombre(p.getNombre()), visitados);
+          boolean res = visitados[devuelvePosNombre(p1.getNombre())];
+          if (res)
+              System.out.printf("%s %d es del mismo grupo que %s %d\n",
+                  p.getNombre(), devuelvePosNombre(p.getNombre()), p1.getNombre(), devuelvePosNombre(p1.getNombre()));
+          else
+              System.out.printf("%s %d NO es del mismo grupo que %s %d\n",
+                  p.getNombre(), devuelvePosNombre(p.getNombre()), p1.getNombre(), devuelvePosNombre(p1.getNombre()));
+          return res;
+      }
+      System.out.println("No existe alguien con ese nombre en la red");
+      return false;
+
   }
     // Apartado 2.2.5 Quinto método
     public void motrarMiembrosGrupo(Persona p){
-        //Completar
+        if (devuelvePosNombre(p.getNombre()) >= 0) {
+            boolean[] visitados = new boolean[getNumPersonas()];
+            for (int i = 0; i < getNumPersonas(); i++)
+                visitados[i] = false;
+            miREd.recorridoEnProfundidadCadena(devuelvePosNombre(p.getNombre()), visitados);
+            for (int i = 0; i < getNumPersonas(); i++)
+                if (visitados[i] && i != devuelvePosNombre(p.getNombre()))
+                    System.out.printf("%d: %s\n", i, contactos[i].getNombre());
+        } else
+            System.out.println("No existe alguien con ese nombre en la red");
     }
 
     // Apartado 2.2.6 sexto método
     public void motrarSeguidosPor(Persona p){
-        //Completar
+        if (devuelvePosNombre(p.getNombre()) >= 0) {
+            boolean[] visitados = new boolean[getNumPersonas()];
+            for (int i = 0; i < getNumPersonas(); i++)
+                visitados[i] = false;
+            miREd.recorridoEnProfundidad(devuelvePosNombre(p.getNombre()), visitados);
+            for (int i = 0; i < getNumPersonas(); i++)
+                if (visitados[i] && i != devuelvePosNombre(p.getNombre()))
+                    System.out.printf("%d: %s\n", i, contactos[i].getNombre());
+        } else
+            System.out.println("No existe alguien con ese nombre en la red");
     }
 
     // Apartado 2.2.7 séptimo método
 
     public boolean mostrarEsSeguidaPor(String nombre1, String nombre2){
-        boolean resul= false;
-        //Completar
-        return resul;
+        boolean[] visitados = new boolean[getNumPersonas()];
+        for (int i = 0; i < getNumPersonas(); i++)
+            visitados[i] = false;
+        miREd.recorridoEnProfundidad(devuelvePosNombre(nombre1), visitados);
+        return visitados[devuelvePosNombre(nombre2)];
     }
 
 
     // Apartado 2.2.8 octavo método (opcional)
    public void mostrarSeguidoresDe(String nombre1){
-       //Completar
-
+       int pos = devuelvePosNombre(nombre1);
+       boolean [] visitados = new boolean[getNumPersonas()];
+       for (int i = 0; i < getNumPersonas(); i++) {
+           visitados = mostrarSeguidoresDeRec(i,visitados);
+           if(visitados[pos] && i!=pos)
+               System.out.printf("%d: %s\n", i, contactos[i].getNombre());
+       }
    }
+    private boolean[] mostrarSeguidoresDeRec(int i, boolean[] visitados ) {
+        for (int j = 0; j < getNumPersonas(); j++)
+            visitados[j] = false;
+        miREd.recorridoEnProfundidad(i, visitados);
+        return visitados;
+    }
 
 }
-	  
-
-
-
-
